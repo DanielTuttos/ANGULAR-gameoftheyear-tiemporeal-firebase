a0d6e989b53ecc72e13cd25084f26289fbb6079b
Daniel Romero AG <48894430+DanielTuttos@users.noreply.github.com>
2020-03-23 19:56:26 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,13 @@
 export const environment = {
   production: true,
-  url: 'https://us-central1-firestore-grafica-avanzado-an.cloudfunctions.net',
+   url: 'http://localhost:5000/firestore-grafica-avanzado-an/us-central1',
   firebase: {
-    apiKey: "AIzaSyBtfUlXYb7XmJgOH__0egURY6sYuVvvBso",
-    authDomain: "firestore-grafica-avanzado-an.firebaseapp.com",
-    databaseURL: "https://firestore-grafica-avanzado-an.firebaseio.com",
-    projectId: "firestore-grafica-avanzado-an",
-    storageBucket: "firestore-grafica-avanzado-an.appspot.com",
-    messagingSenderId: "194511205349",
-    appId: "1:194511205349:web:615964c9ddaae90bdcf50c"
+    apiKey: "---------",
+    authDomain: "--------",
+    databaseURL: "--------",
+    projectId: "-------",
+    storageBucket: "-------",
+    messagingSenderId: "---------------",
+    appId: "----------"
   }
-
 };
